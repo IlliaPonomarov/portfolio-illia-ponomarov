c307ff08c9a387ba6699a356fb1fde2d0bc078b1
Illia <chippaxyucac@gmail.com>
2023-01-20 23:01:50 +0100

    init

---
+++ src/main/java/com/example/portfolioilliaponomarov/PortfolioIlliaPonomarovApplication.java
@@ -0,0 +1,13 @@
+package com.example.portfolioilliaponomarov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioIlliaPonomarovApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortfolioIlliaPonomarovApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/portfolioilliaponomarov/PortfolioIlliaPonomarovApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.portfolioilliaponomarov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioIlliaPonomarovApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
